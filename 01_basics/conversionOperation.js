1fb6e375b7055c08dc6e145acee0714c65269770
Preeti- <159798577+preetisonule@users.noreply.github.com>
2025-01-30 09:41:51 +0000

    Conversion done

---
+++ 01_basics/conversionOperation.js
@@ -0,0 +1,16 @@
+
+// To Number
+let score = "290";
+let value = Number(score);
+console.log(value);
+console.log(typeof value);
+
+// To Boolean
+let isLoggedIn = 1;
+let bolLoggedIn = Boolean(isLoggedIn);
+console.log(bolLoggedIn);
+
+// To String 
+let someNum = 245;
+let nm1 = String(someNum);
+console.log(someNum + " " + typeof(nm1));
