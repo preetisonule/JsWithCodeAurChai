af979a2cef083cb705c30cf57870ed53904322f0
Preeti- <159798577+preetisonule@users.noreply.github.com>
2025-01-30 09:15:13 +0000

    learnt variables and datatypes

---
+++ 01_basics/dataTypes.js
@@ -0,0 +1,13 @@
+"use strict"; // treat all JS code as newer version
+
+// alert(3+3) // we are using node.js not browser
+
+console.log("Preeti")
+
+let nm = "preeti"        // String
+let number = 23            // integer
+let isLoggedIn = false     // boolean
+let ok = null              // null
+let sure                   // undefined
+                           // symbol => React
+console.log(typeof nm);
---
+++ 01_basics/variables.js
@@ -0,0 +1,6 @@
+const marks = 1234;
+let Password = "Preeti12345@";
+// var lass = "A";
+// marks = "90";
+
+console.table([ marks , Password]);
