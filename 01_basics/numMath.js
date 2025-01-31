be26ae04a417fb9169e5dd65beff7afe6d815ce6
Preeti- <159798577+preetisonule@users.noreply.github.com>
2025-01-31 07:18:43 +0000

    number and Math done.

---
+++ 01_basics/numMath.js
@@ -0,0 +1,23 @@
+const balance = new Number(100);
+// console.log(balance);
+
+// console.log(balance.toString().length);
+// console.log(balance.toFixed(1));
+
+const hundreds = 1000000;
+console.log(hundreds.toLocaleString('en-In'));
+
+
+
+
+// console.timeLog(Math.random());                      // returns value from 0 to 1
+// console.log((Math.random()*10) + 1);                 // returns value from 0 to 9 
+//                                                      // by adding 1 gives values from 1 to 10
+// console.log(Math.floor(Math.random()*10)+1);         // floor value
+
+const min = 10;
+const max = 20;             // min 10 chahiye max 20.
+
+// console.log(Math.floor(Math.random() * (max - min + 1)+ min));
+
+
