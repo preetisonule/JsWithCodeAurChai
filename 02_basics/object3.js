c090f796f8ba380979506ffe716fd90943752811
Preeti- <159798577+preetisonule@users.noreply.github.com>
2025-02-01 17:15:42 +0000

    object2 ,destructuring and JSON done.

--- 02_basics/object3.js
+++ 02_basics/object3.js
@@ -4,5 +4,14 @@ const course = {
     instructor : "Hitesh"
 }
 
+// destructuring object
 const {instructor} = course;
 console.log(instructor);
+
+// JSON 
+// Basically JSON is object like format in JS without name.
+// object jaisa hi format hai bro easy weasy.
+// {
+//     name : "preeti",
+//     age : "19"
+// }
